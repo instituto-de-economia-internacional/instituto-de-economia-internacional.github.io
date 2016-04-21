ea735697ca02c81e8f5b39588466640ac95edd36
Paco Garcia <fgtortosa@iei.ua.es>
2016-04-21 10:46:14 +0200

    Cambios breves!

--- helpers/rss_helpers.rb
+++ helpers/rss_helpers.rb
@@ -7,6 +7,13 @@ module RssHelpers
   def rss_noticias_alicante
    rss_read_feed(data.fuentesrss.noticias.ua.first)
   end
+  def rss_noticias_valencia
+   rss_read_feed(data.fuentesrss.noticias.uv.first)
+  end
+  def rss_noticias_castellon
+   rss_read_feed(data.fuentesrss.noticias.uji.first)
+  end
+
   def rss_read_feed(url)
     Feedjira::Feed.fetch_and_parse url
   end
