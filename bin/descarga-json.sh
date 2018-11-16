a95c5c6f92904bd524b515b80078a9264d5ef8c4
Iren <iren@iei.ua.es>
2018-11-16 09:31:54 +0100

    Updating news

--- bin/descarga-json.sh
+++ bin/descarga-json.sh
@@ -1,13 +1,13 @@
 mv data/*.json old-data/
 wget http://iei.ua.es/es/api/investigadores-json.json
-mv investigadores-json.json data/investigadores_ua.json
+mv investigadores-json.json ../data/investigadores_ua.json
 wget http://iei.uv.es/investigadores.php
-mv investigadores.php data/investigadores_uva.json
+mv investigadores.php ../data/investigadores_uva.json
 wget http://iei.uji.es/investigadores.php
-mv investigadores.php data/investigadores_uji.json
+mv investigadores.php ../data/investigadores_uji.json
 wget http://iei.ua.es/es/api/noticias-json.json
-mv noticias-json.json data/noticias_ua.json
+mv noticias-json.json ../data/noticias_ua.json
 wget http://iei.uji.es/noticias.php
-mv noticias.php data/noticias_uji.json
+mv noticias.php ../data/noticias_uji.json
 wget http://iei.uv.es/noticias.php
-mv noticias.php data/noticias_uva.json
+mv noticias.php ../data/noticias_uva.json
